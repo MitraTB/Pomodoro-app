5c48cecdd71dbead5920dbd25ba16557a3a2c778
Mitra Tabatabaei <mitra.tabatabaei.asl@gmail.com>
2020-07-21 10:34:29 +0430

    making some changes in state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React  from "react";
+import React, {useState} from "react";
 import Buttons from "./Buttons";
 import DisplayTime from "./DisplayTime";
 
 //import './App.css';
 
 function App() {
-  //let [result , setResult] = useState("25 : 00");
-  let result;
+  let [result , setResult] = useState("25 : 00");
+  //let result;
   const displayCurrentTimeLeft = (time) => {
     const seconds = time % 60;
     const minutes = Math.floor(time / 60);
-    function addZeroToTime(times) {
+    function  addZeroToTime (times) {
       return times < 10 ? `0${times}` : times;
     }
-    result = `${addZeroToTime(minutes)} : ${addZeroToTime(seconds)}`;
+    //result = `${addZeroToTime(minutes)} : ${addZeroToTime(seconds)}`;
+    setResult(`${addZeroToTime(minutes)} : ${addZeroToTime(seconds)}`);
     console.log(result);
     //console.log(setResult);
      };
